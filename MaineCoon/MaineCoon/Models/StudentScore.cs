8931a12453f6e3de0d71098d4a5c8d4d04008a33
PupilTong <earth.diqiu@gmail.com>
2019-10-07 03:06:34 -0400

    Add DataModel

---
+++ MaineCoon/MaineCoon/Models/StudentScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaineCoon.Models {
+    public class StudentScore {
+        public int id { get; set; }
+        public double  T { get; set; }
+        public double G { get; set; }
+        public double UR { get; set; }
+        public double SOP { get; set; }
+        public double LOR { get; set; }
+        public double GPA { get; set; }
+        public double RES { get; set; }
+    }
+}
---
+++ MaineCoon/MaineCoon/Pages/DataInput/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MaineCoon.Data;
+using MaineCoon.Models;
+
+namespace MaineCoon.Pages.DataInput
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MaineCoon.Data.StudentScoreContext _context;
+
+        public DetailsModel(MaineCoon.Data.StudentScoreContext context)
+        {
+            _context = context;
+        }
+
+        public StudentScore StudentScore { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            StudentScore = await _context.StudentScore.FirstOrDefaultAsync(m => m.id == id);
+
+            if (StudentScore == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
